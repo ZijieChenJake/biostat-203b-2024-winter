88830dcfdf2805a02e579f262d798a05b254e67d
Zijie Chen <zijiechen156@ucla.edu>
2024-03-05 22:57:03 -0800

    Finishing Building Shiny app and waiting to fix input

--- hw4/mimiciv_shiny/app.R
+++ hw4/mimiciv_shiny/app.R
@@ -11,17 +11,16 @@ library(tidyverse)
 library(styler)
 #getwd()
 mimic_icu_cohort <- read_rds("mimic_icu_cohort.rds")
+adt <- tbl(con_bq, "transfers") 
+lab <- tbl(con_bq, "labevents") 
+proc <- tbl(con_bq, "procedures_icd")
+patients <- tbl(con_bq, "patients")
+admission <- tbl(con_bq, "admissions") 
+d_proc <- tbl(con_bq, "d_icd_procedures") 
+dia <- tbl(con_bq, "diagnoses_icd") 
+d_dia <- tbl(con_bq, "d_icd_diagnoses")
 
-#data <- mimic_icu_cohort %>%
- # select(Potassium, Sodium) %>%
-  #pivot_longer(cols = c("Potassium", "Sodium"), 
-   #            names_to = "variable", values_to = "value")
-
-#data %>%
- # ggplot(aes(x = value, y = variable)) +
- #  geom_boxplot(notch=TRUE) +
- # theme_minimal()
-
+####
 ui <- fluidPage(
   tabsetPanel(
     tabPanel("Patient characteristics",
@@ -39,13 +38,11 @@ ui <- fluidPage(
                                         "gender",
                                         "Lab Events" = "labevents",
                                         "Chart Events" = "chartevents")),
-                 actionButton("update", "Update"),
                  checkboxInput("remove", 
                             "Remove outliers in IQR method for measurements?")
                ),
                mainPanel(
-                 plotOutput("summaryPlot"),
-                 tableOutput("summaryTable")
+                 plotOutput("summaryPlot")
                )
              )),
     tabPanel("Patient's ADT and ICU stay information",
@@ -53,83 +50,137 @@ ui <- fluidPage(
                sidebarPanel(
                  helpText("Select a patient."),
                  textInput("patientID", "Patient ID"),
-                 actionButton("lookup", "Lookup")
                ),
                mainPanel(
-                 tableOutput("patientDetails")
+                 plotOutput("patientDetails")
                )
              ))
   )
 )
 server <- function(input, output) {
-  observeEvent(input$update, {
+  dataReactive <- reactive({
     if(input$variable == "labevents"){
       data <- mimic_icu_cohort %>%
         select(Potassium, Sodium, Glucose, Creatinine, Chloride, Bicarbonate) %>%
-        pivot_longer(cols = c("Potassium", "Sodium","Glucose","Creatinine",
-                              "Chloride","Bicarbonate"), 
+        pivot_longer(cols = c("Potassium", "Sodium", "Glucose", "Creatinine"
+                              , "Chloride", "Bicarbonate"), 
                      names_to = "variable", values_to = "value")
-      if(input$remove == FALSE){
-        output$summaryPlot <- renderPlot({
-          data %>%
-            ggplot(aes(x = value,y = variable)) +
-            geom_boxplot(notch=TRUE) +
-            xlim(0,250) +
-            theme_minimal()
-        })}else{
-          output$summaryPlot <- renderPlot({
-            data %>%
-              ggplot(aes(x = value,y = variable)) +
-              geom_boxplot(notch=TRUE,outlier.shape = NA) +
-              xlim(0,250) +
-              theme_minimal()
-          })
-        }
     } else if(input$variable == "chartevents"){
       data <- mimic_icu_cohort %>%
-        select(`Temperature Fahrenheit`,`Non Invasive Blood Pressure diastolic`,
-               `Respiratory Rate`, `Non Invasive Blood Pressure systolic`
-               , `Heart Rate`) %>%
-        pivot_longer(cols = c(`Temperature Fahrenheit`,`Non Invasive Blood Pressure diastolic`,
+        select(`Temperature Fahrenheit`, `Non Invasive Blood Pressure diastolic`
+               , `Respiratory Rate`, 
+               `Non Invasive Blood Pressure systolic`, `Heart Rate`) %>%
+        pivot_longer(cols = c(`Temperature Fahrenheit`, 
+                              `Non Invasive Blood Pressure diastolic`, 
                               `Respiratory Rate`, `Non Invasive Blood Pressure systolic`
                               , `Heart Rate`), 
                      names_to = "variable", values_to = "value")
-      if(input$remove == FALSE){
-        output$summaryPlot <- renderPlot({
-          data %>%
-            ggplot(aes(x = value,y = variable)) +
-            geom_boxplot(notch=TRUE) +
-            xlim(0,250) +
-            theme_minimal()
-        })}else{
-          output$summaryPlot <- renderPlot({
-            data %>%
-              ggplot(aes(x = value,y = variable)) +
-              geom_boxplot(notch=TRUE,outlier.shape = NA) +
-              xlim(0,250) +
-              theme_minimal()
-          })
-        }
-    }else {
+    } else {
       data <- mimic_icu_cohort
-      variable <- input$variable
-      output$summaryPlot <- renderPlot({
-        data %>%
-          ggplot(aes_string(y = variable)) +
-          geom_bar() +
-          theme_minimal()
-      })
     }
+    return(data)
   })
   
-  observeEvent(input$lookup, {
-    patientID <- input$patientID
+  # Plot for the first tab
+  output$summaryPlot <- renderPlot({
+    data <- dataReactive() # Use the reactive data
+    if(input$variable %in% c("labevents", "chartevents")){
+      if(input$remove == TRUE){
+        plot <- ggplot(data, aes(x = value, y = variable)) +
+          geom_boxplot(notch=TRUE, outlier.shape = NA) +
+          xlim(0,250) +
+          theme_minimal()
+      } else {
+        plot <- ggplot(data, aes(x = value, y = variable)) +
+          geom_boxplot(notch=TRUE) +
+          xlim(0,250) +
+          theme_minimal()
+      }
+    } else {
+      variable <- input$variable
+      plot <- ggplot(data, aes_string(y = variable)) +
+        geom_bar() +
+        theme_minimal()
+    }
+    print(plot)
+  })  
+#Second Tab
+    dataReactive2 <- reactive({
+      sid <- as.numeric(input$patientID)
+      sid_info <- patients %>%
+        filter(subject_id == sid) %>%
+        collect()
+      sid_adt <- adt %>%
+        filter(subject_id == sid) %>%
+        collect()
+      sid_adm <- admission %>%
+        filter(subject_id == sid) %>%
+        collect()
+      sid_lab <- lab %>%
+        filter(subject_id == sid) %>%
+        collect()
+      sid_proc <- proc %>%
+        filter(subject_id == sid) %>%
+        left_join(d_proc, by = c("icd_code", "icd_version")) %>%
+        collect()
+      sid_proc$chartdate <- as.POSIXct(sid_proc$chartdate)
+      sid_diag <- dia %>%
+        filter(subject_id == sid) %>%
+        left_join(d_dia, by = c("icd_code", "icd_version")) %>%
+        collect()
+      list(sid = sid, sid_info = sid_info, sid_adt = sid_adt, sid_adm = sid_adm, 
+           sid_lab = sid_lab, sid_proc = sid_proc, sid_diag = sid_diag)
+   })
+    # Plot for the second tab
     output$patientDetails <- renderPlot({
-      mimic_icu_cohort %>%
-        filter(subject_id == patientID) %>%
-        ggplot()
+      data2 <- dataReactive2()
+      ggplot() +
+        geom_segment(
+          data = data2$sid_adt %>% filter(eventtype != "discharge"),
+          mapping = aes(
+            x = intime,
+            xend = outtime,
+            y = "ADT",
+            yend = "ADT",
+            color = careunit,
+            linewidth = str_detect(careunit, "(ICU|CCU)")
+          ),
+       ) +
+      geom_point(
+        data = data2$sid_lab %>% distinct(charttime, .keep_all = TRUE),
+        mapping = aes(x = charttime, y = "Lab"),
+        shape = '+',
+        size = 5
+      ) +
+      geom_jitter(
+        data = data2$sid_proc,
+        mapping = aes(
+          x = chartdate,
+          y = "Procedure",
+          shape = str_sub(long_title, 1, 25)
+        ),
+        size = 3,
+        height = 0
+      ) + 
+      labs(
+        title = str_c(
+          "Patient ", data2$sid, ", ",
+          data2$sid_info$gender, ", ",
+          data2$sid_info$anchor_age + year(data2$sid_adm$admittime[1]) - data2$sid_info$anchor_year,
+          " years old, ",
+          str_to_lower(data2$sid_adm$race[1])
+        ),
+        subtitle = str_c(str_to_lower(data2$sid_diag$long_title[1:3]), collapse = "\n"),
+                         x = "Calendar date",
+                         y = "",
+                         color = "Care unit",
+                         shape = "Procedure"
+                         ) +
+        guides(linewidth = "none") +
+        scale_y_discrete(limits = rev) +
+        theme_light() +
+        theme(legend.position = "bottom", legend.box = "vertical")
     })
-  })
 }
 
 shinyApp(ui, server)
